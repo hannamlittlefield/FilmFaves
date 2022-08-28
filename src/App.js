8bb2ddb5162735bc7fcfe9b805a13097b5eff38b
Hanna <hannamlittlefield@gmail.com>
2022-08-28 18:03:08 -0400

    adding search box and functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './Components/MovieList';
+import MovieListHeading from './Components/MovieListHeading';
+import SearchBox from './Components/SearchBox';
 
 const App = () => {
+  //Change the apiKey variable to your personal API key
+  let apiKey = '123114ca'
 	const [movies, setMovies] = useState([])
+  const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
-		const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apiKey}`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -18,12 +23,16 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		getMovieRequest();
-	}, []);
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
 
 	return (
-		<div className='container-fluid movie-app'>
+		<div className='container-fluid film-faves-project'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Films' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+  </div>
 			<div className='row'>
         <MovieList movies={movies} />
 			</div>
