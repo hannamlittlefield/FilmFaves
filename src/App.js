1819d6848dca762d9e41b7ee0753eb3fae9be611
Hanna <hannamlittlefield@gmail.com>
2022-08-28 18:54:24 -0400

    adding favorite functionality and code

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './Components/MovieList';
-import MovieListHeading from './Components/MovieListHeading';
 import SearchBox from './Components/SearchBox';
+import MovieListHeading from './Components/MovieListHeading';
+import AddFavourites from './Components/AddFavourites';
+import RemoveFavourites from './Components/RemoveFavourites';
 
 const App = () => {
-  //Change the apiKey variable to your personal API key
+  //Change this to your personal API key
   let apiKey = '123114ca'
-	const [movies, setMovies] = useState([])
-  const [searchValue, setSearchValue] = useState('');
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async () => {
+	const getMovieRequest = async (searchValue) => {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apiKey}`;
 
 		const response = await fetch(url);
@@ -26,15 +29,56 @@ const App = () => {
 		getMovieRequest(searchValue);
 	}, [searchValue]);
 
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
 
 	return (
-		<div className='container-fluid movie-grid'>
-      <div className='row d-flex align-items-center mt-4 mb-4'>
-        <MovieListHeading heading='Films' />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-  </div>
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
 			<div className='row'>
-        <MovieList movies={movies} />
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					favouriteComponent={AddFavourites}
+					handleFavouritesClick={addFavouriteMovie}
+				/>
 			</div>
 		</div>
 	);
